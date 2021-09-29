412279200974bbc1e7bc980822959b779b70c404
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-29 15:35:50 -0400

    :art: formatea código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
