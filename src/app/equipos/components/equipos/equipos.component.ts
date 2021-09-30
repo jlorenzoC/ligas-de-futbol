ac2a08cf30a50d2afd9bc913fe3018a3681d3f99
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-30 15:57:58 -0400

    :heavy_plus_sign: tabla para visualizar los equipos
    Agraga tabla para visualizar los equipos pertenecientes a una liga

--- src/app/equipos/components/equipos/equipos.component.ts
+++ src/app/equipos/components/equipos/equipos.component.ts
@@ -1,15 +1,29 @@
+import { LigasService } from './../../../services/ligas.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { Equipo } from '../../models/equipo.model';
+import { EquiposService } from './../../services/equipos.service';
 
 @Component({
   selector: 'app-equipos',
   templateUrl: './equipos.component.html',
-  styleUrls: ['./equipos.component.scss']
+  styleUrls: ['./equipos.component.scss'],
 })
 export class EquiposComponent implements OnInit {
+  equipos: Observable<Equipo[]> = of([]);
+  equipoSeleccionado = new Equipo();
+  nombreDeLaLiga: Observable<string> = of('');
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private equiposService: EquiposService,
+    private ligasService: LigasService
+  ) {}
 
   ngOnInit(): void {
+    const idDeLaLiga = this.route.snapshot.params.idDeLaLiga;
+    this.nombreDeLaLiga = this.ligasService.getNombreDeLaLiga(idDeLaLiga);
+    this.equipos = this.equiposService.getEquiposPorLiga(idDeLaLiga);
   }
-
 }
--- src/app/equipos/equipos.module.ts
+++ src/app/equipos/equipos.module.ts
@@ -3,14 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { EquiposRoutingModule } from './equipos-routing.module';
 import { EquiposComponent } from './components/equipos/equipos.component';
+import { TableModule } from 'primeng/table';
+import { AvatarModule } from 'primeng/avatar';
 
 @NgModule({
-  declarations: [
-    EquiposComponent
-  ],
-  imports: [
-    CommonModule,
-    EquiposRoutingModule
-  ]
+  declarations: [EquiposComponent],
+  imports: [CommonModule, EquiposRoutingModule, TableModule, AvatarModule],
 })
-export class EquiposModule { }
+export class EquiposModule {}
