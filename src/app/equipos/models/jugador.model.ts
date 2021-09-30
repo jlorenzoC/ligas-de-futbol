8252ae1c10e552be0c7279255064cd6d8023db54
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-30 16:54:36 -0400

    :sparkles: muestra los jugadores de un equipo

---
+++ src/app/equipos/models/jugador.model.ts
@@ -0,0 +1,6 @@
+export class Jugador {
+  'Nombre del Jugador': string;
+  'id': string;
+  'Avatar': string;
+  'teamId': string;
+}
---
+++ src/app/equipos/services/jugadores.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Equipo } from '../models/equipo.model';
+import { Jugador } from '../models/jugador.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JugadoresService {
+  constructor(private http: HttpClient) {}
+
+  getJugadoresDelEquipo(idDelEquipo: string): Observable<Jugador[]> {
+    return this.http.get<Jugador[]>(`players?teamId=${idDelEquipo}`);
+  }
+}
