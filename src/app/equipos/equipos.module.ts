41a8f13c6529207508ae40024ae62a92959ce9aa
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-30 10:34:22 -0400

    :heavy_plus_sign: crea componente para equipos

--- src/app/equipos/equipos.module.ts
+++ src/app/equipos/equipos.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EquiposRoutingModule } from './equipos-routing.module';
+import { EquiposComponent } from './components/equipos/equipos.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, EquiposRoutingModule],
+  declarations: [
+    EquiposComponent
+  ],
+  imports: [
+    CommonModule,
+    EquiposRoutingModule
+  ]
 })
-export class EquiposModule {}
+export class EquiposModule { }
