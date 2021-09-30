a830c2d91cc18e0358e6626f6104d6a4c7173138
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-29 22:06:41 -0400

    :heavy_plus_sign: usa servicio para obtener ligas
    Solo hace una prueba para usar el servicio, obtene las ligas y listar el nombre de las ligas.

--- src/app/components/ligas/ligas.component.ts
+++ src/app/components/ligas/ligas.component.ts
@@ -1,15 +1,19 @@
+import { Liga } from '../../models/liga.model';
+import { LigasService } from './../../services/ligas.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-ligas',
   templateUrl: './ligas.component.html',
-  styleUrls: ['./ligas.component.sass']
+  styleUrls: ['./ligas.component.sass'],
 })
 export class LigasComponent implements OnInit {
+  ligas: Observable<Liga[]> = of([]);
 
-  constructor() { }
+  constructor(private ligasService: LigasService) {}
 
   ngOnInit(): void {
+    this.ligas = this.ligasService.getLigas();
   }
-
 }
