c13af8d2e95235bef2ee44cefa9d8536c242672a
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-29 21:18:42 -0400

    ➕ adiciona rutas
    Adiciona rutas hacia componente LigasComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { LigasComponent } from './components/ligas/ligas.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'ligas' },
+  { path: 'ligas', component: LigasComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
