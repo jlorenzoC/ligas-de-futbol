71c961463f2f8c5b60629d97b326710a9eb078e6
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-30 10:36:18 -0400

    :heavy_plus_sign: adiciona rutas
    adiciona rutas hacia el componente de equipos

--- src/app/equipos/equipos-routing.module.ts
+++ src/app/equipos/equipos-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EquiposComponent } from './components/equipos/equipos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'equipos' },
+  { path: 'equipos', component: EquiposComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
