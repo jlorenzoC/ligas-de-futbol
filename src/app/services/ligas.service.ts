e71770175fdc2f3e0af67a495e4b3dff4ae99569
Javier Lorenzo <jacksparrow2017.11.30@gmail.com>
2021-09-29 22:04:37 -0400

    :heavy_plus_sign: actualiza LigasService
    Adiciona método para obtener de la api, las ligas.

--- src/app/services/ligas.service.ts
+++ src/app/services/ligas.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Liga } from '../models/liga.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LigasService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getLigas(): Observable<Liga[]> {
+    return this.http.get<Liga[]>('leagues');
+  }
 }
